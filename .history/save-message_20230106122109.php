559efb22ce00be3fb8a39ffafb0f04c26a0b5d5c
pilar122 <albapilarte@gmail.com>
2023-01-06 12:52:35 -0700

    .history

--- .history/save-message_20230106122109.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $message = $_POST['message'];
-  file_put_contents('messages.txt', $message . "\n", FILE_APPEND);
-?>
--- .history/save-message_20230106122822.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $message = $_POST['message'];
-  file_put_contents('messages.txt', $message . "\n", FILE_APPEND);
-?>
-
-<?php
-  $messages = file_get_contents('messages.txt');
-?>
