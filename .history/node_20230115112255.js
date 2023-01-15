0119c7daf1fe880e88b611dd6028442e00028030
pilar122 <albapilarte@gmail.com>
2023-01-15 11:41:32 -0700

    commit message from terminal

---
+++ .history/node_20230115112255.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+//set up an array to store comments 
+let comments = [];
+
+//set up routes 
+app.get('/',(req,res)=> {
+    res.send('Welcome to Tychaios!');
+});
+
+app.get('/comments',(req,res)=> {
+    res.json(comments);
+});
+
+app.post('/comments',(req,res)=> {
+    //Get comment from request body 
+    const comment = req.body;
+
+    //add comment to array
+    comments.push(comment);
+
+    //send response
+    res.json({message:'Comment added!'});
+});
+
+//listen for requests
+app.listen(3000,() => {
+    console.log('Comment app listening on port 3000');
+});
